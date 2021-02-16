861b83f8a51f9dd60259031dc3a2f30304297ea2
YuliaLulis <lulis_job@mail.ru>
2021-02-16 20:50:06 +0300

    fix: add implementation of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    var splitedArray = String(n).split("");
+            
+            if (splitedArray.length == 3) {
+                if (splitedArray[2] == 0)
+                    return splitedArray.splice(0, 2).reverse().join("");
+            }
+
+
+            if (splitedArray.length == 4) {
+                if (splitedArray[0] != 0)
+                    return splitedArray.splice(1, 3).reverse().join("");
+            }
+
+            if (splitedArray[0] > 0) {
+                return splitedArray.reverse().join("");
+            }
+
 }
